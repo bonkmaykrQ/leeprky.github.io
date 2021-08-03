3765eb7e1fd6a128fb8de9f77985abc3bb3e9fc6
Lee <71296232+leeprky@users.noreply.github.com>
2021-08-03 14:47:17 +0100

    Update AllInOneInstallerGlasscord.bat

--- files/AllInOneInstallerGlasscord.bat
+++ files/AllInOneInstallerGlasscord.bat
@@ -13,11 +13,34 @@ cd powercord/src/Powercord/plugins && git clone https://github.com/LandenStephen
 cd ..\..\..\..\
 cd powercord/src/Powercord/themes && git clone https://github.com/leeprky/Ovel
 taskkill /IM DiscordCanary.exe /F
+timeout 3 /nobreak
 start %USERPROFILE%\AppData\Local\DiscordCanary\Update.exe --processStart DiscordCanary.exe
+mkdir %USERPROFILE%\AppData\Local\DiscordCanary\app-1.0.37\resources\app\Backup\Files"
+xcopy "%USERPROFILE%\AppData\Local\DiscordCanary\app-1.0.37\resources\app" "%USERPROFILE%\AppData\Local\DiscordCanary\app-1.0.37\resources\app\Backup\Files"
+cd %USERPROFILE%\AppData\Local\DiscordCanary\app-1.0.37\resources\app\Backup
+git clone https://github.com/leeprky/RestoreGit
+xcopy "%USERPROFILE%\AppData\Local\DiscordCanary\app-1.0.37\resources\app\Backup\RestoreGit\restore.bat" "%USERPROFILE%\AppData\Local\DiscordCanary\app-1.0.37\resources\app"
+del /f /q "%USERPROFILE%\AppData\Local\DiscordCanary\app-1.0.37\resources\app\Backup\RestoreGit*.*"
+for /d %%d in ("%USERPROFILE%\AppData\Local\DiscordCanary\app-1.0.37\resources\app\Backup*.*") do rmdir /s /q "%%d"
+@Echo Off
+SETLOCAL EnableDelayedExpansion
+for /F "tokens=1,2 delims=#" %%a in ('"prompt #$H#$E# & echo on & for %%b in (1) do     rem"') do (
+  set "DEL=%%a"
+)
+call :colorEcho a0 "BACKUP CREATED IF FILES COPIED"
+echo.
+exit
+:colorEcho
+echo off
+<nul set /p ".=%DEL%" > "%~2"
+findstr /v /a:%1 /R "^$" "%~2" nul
+del "%~2" > nul 2>&1i
+timeout 3 /nobreak
 rem 
 set "FirstDir=%USERPROFILE%\AppData\Local\DiscordCanary\app-1.0.37\resources\app\GlasscordHelper"
 set "SecondDir=%USERPROFILE%\AppData\Local\DiscordCanary\app-1.0.37\resources\app\GlasscordHelper"
 rem 
+
 if exist "%FirstDir\" (
     rem 
     mkdir "%SecondDir%"
@@ -47,4 +70,5 @@ echo off
 findstr /v /a:%1 /R "^$" "%~2" nul
 del "%~2" > nul 2>&1i
 timeout 10 /nobreak
-pause
+@Echo Off
+quit
