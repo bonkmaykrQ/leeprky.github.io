8bbae5f3fea17eda3278e11e91b626be542e1ac9
Lee <71296232+leeprky@users.noreply.github.com>
2021-08-26 15:32:45 +0100

    Add files via upload

--- files/AllInOneInstaller+Glasscord.bat
+++ files/AllInOneInstaller+Glasscord.bat
@@ -1,29 +1,23 @@
-mkdir %USERPROFILE%\AppData\Local\DiscordCanary\app-1.0.37\resources\app\Backup\Files"
-xcopy "%USERPROFILE%\AppData\Local\DiscordCanary\app-1.0.37\resources\app" "%USERPROFILE%\AppData\Local\DiscordCanary\app-1.0.37\resources\app\Backup\Files"
-cd %USERPROFILE%\AppData\Local\DiscordCanary\app-1.0.37\resources\app\Backup
-git clone https://github.com/leeprky/RestoreGit
-xcopy "%USERPROFILE%\AppData\Local\DiscordCanary\app-1.0.37\resources\app\Backup\RestoreGit\restore.bat" "%USERPROFILE%\AppData\Local\DiscordCanary\app-1.0.37\resources\app"
-del /f /q "%USERPROFILE%\AppData\Local\DiscordCanary\app-1.0.37\resources\app\Backup\RestoreGit*.*"
-for /d %%d in ("%USERPROFILE%\AppData\Local\DiscordCanary\app-1.0.37\resources\app\Backup\RestoreGit*.*") do rmdir /s /q "%%d"
-@Echo Off
-SETLOCAL EnableDelayedExpansion
-for /F "tokens=1,2 delims=#" %%a in ('"prompt #$H#$E# & echo on & for %%b in (1) do     rem"') do (
-  set "DEL=%%a"
-)
-call :colorEcho a0 "BACKUP CREATED IF FILES COPIED"
-echo.
-exit
-:colorEcho
-echo off
-<nul set /p ".=%DEL%" > "%~2"
-findstr /v /a:%1 /R "^$" "%~2" nul
-del "%~2" > nul 2>&1i
-timeout 3 /nobreak
+echo Installer By Leeprky#2063
+cd %USERPROFILE%
+git clone https://github.com/powercord-org/powercord
+cd powercord
+npm i
+npm run plug && pause
+cd %USERPROFILE%
+cd powercord/src/Powercord/plugins && git clone https://github.com/redstonekasi/theme-toggler
+cd ..\..\..\..\
+cd powercord/src/Powercord/plugins && git clone https://github.com/ploogins/PowercordThemeDownloader
+cd ..\..\..\..\
+cd powercord/src/Powercord/plugins && git clone https://github.com/LandenStephenss/PowercordPluginDownloader
+cd ..\..\..\..\
+cd powercord/src/Powercord/themes && git clone https://github.com/leeprky/Ovel
+taskkill /IM DiscordCanary.exe /F
+start %USERPROFILE%\AppData\Local\DiscordCanary\Update.exe --processStart DiscordCanary.exe
 rem 
 set "FirstDir=%USERPROFILE%\AppData\Local\DiscordCanary\app-1.0.37\resources\app\GlasscordHelper"
 set "SecondDir=%USERPROFILE%\AppData\Local\DiscordCanary\app-1.0.37\resources\app\GlasscordHelper"
 rem 
-
 if exist "%FirstDir\" (
     rem 
     mkdir "%SecondDir%"
@@ -53,3 +47,4 @@ echo off
 findstr /v /a:%1 /R "^$" "%~2" nul
 del "%~2" > nul 2>&1i
 timeout 10 /nobreak
+pause
--- files/PowercordInstaller.bat
+++ files/PowercordInstaller.bat
@@ -4,7 +4,7 @@ cd %USERPROFILE%
 git clone https://github.com/powercord-org/powercord
 cd powercord
 npm i
-npm run plug && pause
+npm run plug
 taskkill /IM DiscordCanary.exe /F
 start %USERPROFILE%\AppData\Local\DiscordCanary\Update.exe --processStart DiscordCanary.exe
 pause
