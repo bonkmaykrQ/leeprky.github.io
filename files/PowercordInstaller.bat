99b7f3994ee00ae023809eb0491f904266f45b12
Lee <71296232+leeprky@users.noreply.github.com>
2021-08-30 11:41:02 +0100

    Add files via upload

--- files/PowercordInstaller.bat
+++ files/PowercordInstaller.bat
@@ -4,7 +4,7 @@ cd %USERPROFILE%
 git clone https://github.com/powercord-org/powercord
 cd powercord
 npm i
-npm run plug
+npm run plug && pause
 taskkill /IM DiscordCanary.exe /F
 start %USERPROFILE%\AppData\Local\DiscordCanary\Update.exe --processStart DiscordCanary.exe
 pause
